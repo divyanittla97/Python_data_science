3c95f4a66241d8a649879609f010a992d983e503
divyanittla97 <dnittla@uwaterloo.ca>
2017-10-23 19:24:58 -0400

    Create ProbabilityPractice.py
    Find the probability for shopping

---
+++ ProbabilityPractice.py
@@ -0,0 +1,13 @@
+from numpy import random
+random.seed(0)
+
+totals = {20:0, 30:0, 40:0, 50:0, 60:0}
+purchases = {20:0, 30:0, 40:0, 50:0, 60:0}
+totalPurchases = 0
+for _ in range(100000):
+    ageGroup = random.choice([20, 30, 40, 50, 60, 70])
+    purchaseProbability = 0.4
+    totals[ageGroup] += 1
+    if (random.random() < purchaseProbability):
+        totalPurchases += 1
+        purchases[ageGroup] += 1
